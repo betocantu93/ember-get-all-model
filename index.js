3a541e8097167ad408dcf5f12f07087e5d6d1f4f
Alberto Cantú Gómez <betocantu93@gmail.com>
2018-03-14 16:47:03 -0600

    addon index.js changed and versioning

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-get-all-model'
+  name: 'ember-get-all-model',
+
+  contentFor: function(type, config) {
+    if (type === 'body-footer') {
+      var emberBasicDropdown = this.addons.filter(function(addon) {
+        return addon.name === 'ember-power-select';
+      })[0];
+      return emberBasicDropdown.contentFor(type, config);
+    } else {
+      return '';
+    }
+  }
+
 };
